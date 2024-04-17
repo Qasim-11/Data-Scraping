9a0190e291681421e2fa55f4776a12ed075b9683
Qasim <119961870+Qasim-11@users.noreply.github.com>
2024-04-17 16:56:15 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,60 @@
+from openai import OpenAI 
+
+from langchain.vectorstores.cassandra import Cassandra
+from langchain.indexes.vectorstore import VectorstoreIndexCreator
+from langchain.llms import OpenAI 
+from langchain_community.embeddings import OpenAIEmbeddings
+
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+
+from datasets import load_dataset
+
+ASTRA_DB_SECURE_CONNECT_BUNDLE = "\\Users\\mmahf\\Desktop\\Coding\\Randoms\\AstraDB\\secure-connect-vectordb.zip"
+
+
+ASTRA_CLIENT_ID = "JjmPfnAKGnKAntDGZUbvPleM"
+ASTRA_CLIENT_SECRET = "epQnR.8p4QxMQ3hx8y29ZkNuDZ+k0HGEifLTeoYlxUdPpdWhK+CJ-d0iXB7nd0UwSZkpTZAepPY6pGd_uXPfRRLDq5kFBZ2g8hZAvYMTxofKU37cl63c5p25qhctL+NU"
+ASTRA_TOKEN = "AstraCS:JjmPfnAKGnKAntDGZUbvPleM:0fd2958b0b17a1be6880ceb20b694b666af995677fa27f06474420ceffe2d21f"
+
+ASTRA_DB_KEYSPACE = "default_keyspace"
+OPENAI_API_KEY = "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"
+
+
+
+
+cloud_config= {
+        'secure_connect_bundle': ASTRA_DB_SECURE_CONNECT_BUNDLE
+}
+
+auth_provider = PlainTextAuthProvider(ASTRA_CLIENT_ID, ASTRA_CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+astra_session = cluster.connect()
+
+llm = OpenAI(OPENAI_API_KEY)
+
+embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
+
+cassandra = Cassandra(
+    embedding=embeddings,
+    session=astra_session,
+    keyspace=ASTRA_DB_KEYSPACE,
+    table_name="table"
+)
+
+
+
+print("Loading data from Hugging Face")
+dataset = load_dataset("Biddls/Onion_News" , split = "train")
+headlines = dataset["text"][:50]
+
+
+print("Generating embeddings")
+cassandra.add_texts(headlines)
+
+
+
+print("inserted headlines:" , len(headlines))
+
+
+# I have no idea what I'm doing here, I just wrote this code from a youtube video.
